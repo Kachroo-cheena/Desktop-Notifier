ee04bf476985eb7be54cfe3b3af7f6738e3bbd50
Cheena Kachroo <cheenakachroo7343@gmail.com>
2022-02-25 17:45:14 +0530

    Desktop

---
+++ desktop notifier.py
@@ -0,0 +1,11 @@
+from plyer import notification
+
+title= "Hello everyone"
+message= "Dont forget to drink water after every 10 minutes"
+
+notification.notify(title=title,message=message , timeout= 10, toast= False, app_icon=  None)
+
+# ip= input(int())
+
+# print("your number is:", ip+10)
+
